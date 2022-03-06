9e58d4a98bf15cde0e5cc989a57417d6eb1d68c6
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-03-06 22:55:36 +0200

    can fix this

--- main.js
+++ main.js
@@ -8,19 +8,27 @@ const calculate = () => {
     let result;
     let quote = "Your BMI is: "
     
-    if (BMI < 18.5) {
-        result = `${quote} ${BMI} = "Underweight"`;  
-        color = "warning";    
-    } else if (BMI > 30.0){
-        result = `${quote} ${BMI} = "Obesity"`; 
-        color = "danger";
-    } else if (BMI > 25.0 && BMI < 29.9) {
-        result = `${quote} ${BMI} = "Pre-obesity"`;
-        color = "warning" 
-    } else {
-        result = `${quote} ${BMI} = "Normal or Healthy Weight"`
-        color = "normal" 
+    solution.setAttribute("class", "");
+    if (isNaN(weight) || isNaN(height)){
+        solution.textContent = "no data";
+    }else{
+        if (BMI < 18.5) {
+            result = `${quote} ${BMI} = "Underweight"`;  
+            color = "warning";    
+        } else if (BMI > 30.0){
+            result = `${quote} ${BMI} = "Obesity"`; 
+            color = "danger";
+        } else if (BMI > 25.0 && BMI < 29.9) {
+            result = `${quote} ${BMI} = "Pre-obesity"`;
+            color = "warning" 
+        } else {
+            result = `${quote} ${BMI} = "Normal or Healthy Weight"`
+            color = "normal" 
+        }
+        solution.classList.add(color);
+        solution.textContent = result;
+        document.querySelector("#weight").value = 0;
+        document.querySelector("#height").value = 0;
     }
-    solution.textContent = result;
-    solution.classList.add(color);
+    
 }
